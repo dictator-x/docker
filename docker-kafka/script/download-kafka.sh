1dc676067b9c7670fde072fc2b3834e1e6e12051
dictator-x <dictator.xwu@gmail.com>
2020-06-26 18:32:36 -0400

    format project

--- docker-kafka/script/download-kafka.sh
+++ docker-kafka/script/download-kafka.sh
@@ -4,14 +4,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-FILENAME="kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
-
-url=$(curl --stderr /dev/null "https://www.apache.org/dyn/closer.cgi?path=/kafka/${KAFKA_VERSION}/${FILENAME}&as_json=1" | jq -r '"\(.preferred)\(.path_info)"')
+url=$(curl --stderr /dev/null "https://www.apache.org/dyn/closer.cgi?path=/kafka/${KAFKA_VERSION}/${KAFKA_FILE}&as_json=1" | jq -r '"\(.preferred)\(.path_info)"')
 
 if [[ ! $(curl -s -f -I "${url}") ]]; then
     echo "Mirror does not have desired version, downloading direct from Apache"
-    url="https://archive.apache.org/dist/kafka/${KAFKA_VERSION}/${FILENAME}"
+    url="https://archive.apache.org/dist/kafka/${KAFKA_VERSION}/${KAFKA_FILE}"
 fi
 
 echo "Downloading Kafka from $url"
-wget "${url}" -O "${SOFTWARE_HOME}/${FILENAME}"
+wget "${url}" -O "${SOFTWARE_HOME}/${KAFKA_FILE}"
