cc5d1b37d65fed552d547d1ef0e5d816d35d0c4b
dictator-x <dictator.xwu@gmail.com>
2020-06-27 19:51:29 -0400

    add flume config

--- docker-flume/script/flume-start.sh
+++ docker-flume/script/flume-start.sh
@@ -6,8 +6,8 @@ set -o pipefail
 
 # test_file=${LOG_HOME}/logger
 # touch ${test_file}
-CONFIG_FILE=$1
+AGENT_NAME=$1
 
 # flume-ng agent --name a1 --conf $FLUME_HOME/conf --conf-file $FLUME_HOME/conf/exec-memory-logger.conf -Dflume.root.logger=INFO,console
-flume-ng agent --name a1 --conf $FLUME_HOME/conf --conf-file $FLUME_HOME/conf/$CONFIG_FILE -Dflume.root.logger=INFO,console
+flume-ng agent --name $AGENT_NAME --conf $FLUME_HOME/conf --conf-file $FLUME_HOME/conf/$AGENT_NAME.conf -Dflume.root.logger=INFO,console
 
